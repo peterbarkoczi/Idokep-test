8cb20638996cc206ad6fa7789419bfca3e79d425
peterbarkoczi <barkopetya@gmail.com>
2020-01-08 13:04:18 +0100

    create pages and test for "see product details"

--- src/main/java/com/codecool/idokep/test/pages/WebshopPage.java
+++ src/main/java/com/codecool/idokep/test/pages/WebshopPage.java
@@ -1,4 +1,30 @@
 package com.codecool.idokep.test.pages;
 
-public class WebshopPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class WebshopPage extends Page {
+
+    @FindBy(className = "carousel_prev")
+    WebElement browsePrev;
+
+    @FindBy(className = "carousel_next")
+    WebElement browseNext;
+
+    @FindBy(xpath = "//img[@alt='Kültéri IP kamera']")
+    WebElement cameraThumbnail;
+
+    public WebshopPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void navigateToWebshop() {
+        driver.get("https://shop.idokep.hu");
+    }
+
+    public void clickOnProductThumbnail() {
+        cameraThumbnail.click();
+    }
+
 }
