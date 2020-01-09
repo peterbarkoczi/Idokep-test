89d36e84811a08dce2ef38cccd2b19bf9c8a6486
peterbarkoczi <barkopetya@gmail.com>
2020-01-09 09:39:58 +0100

    create pages and test for "browse next product"

--- src/main/java/com/codecool/idokep/test/pages/WebshopPage.java
+++ src/main/java/com/codecool/idokep/test/pages/WebshopPage.java
@@ -4,16 +4,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class WebshopPage extends Page {
+import java.util.List;
 
-    @FindBy(className = "carousel_prev")
-    WebElement browsePrev;
+public class WebshopPage extends Page {
 
     @FindBy(className = "carousel_next")
     WebElement browseNext;
 
     @FindBy(xpath = "//img[@alt='Kültéri IP kamera']")
     WebElement cameraThumbnail;
+
+    @FindBy(xpath = "//li[@class='ajax_block_product item']")
+    List<WebElement> products;
 
     public WebshopPage(WebDriver driver) {
         super(driver);
@@ -26,5 +28,14 @@ public class WebshopPage extends Page {
     public void clickOnProductThumbnail() {
         cameraThumbnail.click();
     }
+
+    public void clickOnNextArrow() {
+        browseNext.click();
+    }
+
+    public boolean isNextElementDisplayed() {
+        return products.get(4).isDisplayed();
+    }
+
 
 }
